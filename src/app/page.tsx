7f966626a42968a280f5c8b7b282ba4ce460c396
Matheus <lima.eca@gmail.com>
2024-04-30 18:03:17 -0300

    home page with getServerSession verification (server rendered page)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
+import { getServerSession } from "next-auth";
+
+export default async function Home() {
+  const session = await getServerSession();
+  const name = session?.user?.name;
   return (
-    <div>
-      Home
-    </div>
+    <>
+      <div>
+        Home
+      </div>
+      <span>{`Hello ${name ? name : 'stranger'}!`}</span>
+    </>
   );
 }
