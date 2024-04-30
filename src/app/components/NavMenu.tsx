a669f6c9c337d6fa655728ef9092c1f9647a8026
Matheus <lima.eca@gmail.com>
2024-04-30 17:55:50 -0300

    NavMenu with useSession (client component)

---
+++ src/app/components/NavMenu.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+
+function AuthButton() {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <>
+        {session?.user?.name} <br />
+        <button onClick={() => signOut()}>Sign out</button>
+      </>
+    )
+  }
+
+  return (
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn('github')}>Sign in</button>
+    </>
+  )
+}
+
+export default function NavMenu() {
+  return (
+    <div>
+      <AuthButton />
+    </div>
+  );
+}
